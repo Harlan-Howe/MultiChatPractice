45ca930a4670d36db4f5ab2fd654cc9bf4836e83
Harlan Howe <hhowe7@gatech.edu>
2022-10-14 15:06:52 -0500

    PEP compliance Set initial values of method handles self.shut_down_socket and self.message_sender to None.

--- ClientGUIFile.py
+++ ClientGUIFile.py
@@ -2,31 +2,32 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 from typing import List
+
+
 class ClientGUI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Client")
         self.root.geometry('800x800+50+50')
-        self.root.columnconfigure(0,weight=1)
-        self.root.columnconfigure(1,weight=3)
-        self.root.rowconfigure(0,weight=6)
-        self.root.rowconfigure(1,weight=1)
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=3)
+        self.root.rowconfigure(0, weight=6)
+        self.root.rowconfigure(1, weight=1)
 
         bottom_frame = ttk.Frame(self.root)
-        bottom_frame.columnconfigure(0,weight=1)
-        bottom_frame.columnconfigure(1,weight=6)
+        bottom_frame.columnconfigure(0, weight=1)
+        bottom_frame.columnconfigure(1, weight=6)
         bottom_frame.grid(column=0, row=1, columnspan=2, sticky='new')
 
         entry_label = ttk.Label(bottom_frame, text='Enter what you want to say here:')
         entry_label.grid(column=0, row=0,  sticky='e')
-
 
         self.user_entry_string = tk.StringVar()
         self.text_field = ttk.Entry(bottom_frame, textvariable=self.user_entry_string)
         self.text_field.grid(column=1, row=0, sticky='ew')
         self.text_field.bind('<Return>', self.respond_to_text_entry)
 
-        self.user_list_text = ScrolledText(self.root, width = 20, background="black", foreground="white")
+        self.user_list_text = ScrolledText(self.root, width=20, background="black", foreground="white")
         self.user_list_text.grid(column=0, row=0, sticky='ns')
         self.user_list_text['state'] = 'disabled'
 
@@ -34,6 +35,9 @@ class ClientGUI:
         self.chat_response_text = ScrolledText(self.root, background='#bbbbbb')
         self.chat_response_text.grid(column=1, row=0, sticky='ns')
         self.chat_response_text['state'] = 'disabled'
+
+        self.shut_down_socket = None
+        self.message_sender = None
 
     def run_loop(self):
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -43,21 +47,20 @@ class ClientGUI:
         self.shut_down_socket()
         self.root.destroy()
 
-    def set_user_list(self, users: List[str])->None:
+    def set_user_list(self, users: List[str]) -> None:
         names = ""
         for user in users:
             names += f"{user}\n"
 
         self.user_list_text['state'] = 'normal'
-        self.user_list_text.replace(1.0,'end',names)
+        self.user_list_text.replace(1.0, 'end', names)
         self.user_list_text['state'] = 'disabled'
 
-    def add_to_chat(self, entry: str)->None:
+    def add_to_chat(self, entry: str) -> None:
         self.chat_so_far += entry+"\n"
-        self.chat_response_text['state'] ='normal'
-        self.chat_response_text.replace(1.0,'end',self.chat_so_far)
-        self.chat_response_text['state'] ='disabled'
-
+        self.chat_response_text['state'] = 'normal'
+        self.chat_response_text.replace(1.0, 'end', self.chat_so_far)
+        self.chat_response_text['state'] = 'disabled'
 
     def respond_to_text_entry(self, event_info):
         message = self.user_entry_string.get()
