78d1f7e9d1db4d2b4a59ec9aaf9894173401c3ee
Harlan Howe <hhowe7@gatech.edu>
2022-10-14 14:30:28 -0500

    Documentation PEP compliance, where sensible...

--- SocketHostFile.py
+++ SocketHostFile.py
@@ -1,67 +1,72 @@
-import socket,threading, struct
-from typing import Dict, List
+import socket
+import threading
+from typing import Dict
+
 from SocketMessageIOFile import SocketMessageIO, MessageType
 
 
-def broadcast_message_to_all(message:str, message_type=MessageType.MESSAGE):
+def broadcast_message_to_all(message: str, message_type=MessageType.MESSAGE):
     global user_dictionary_lock, user_dictionary, broadcast_manager
     if broadcast_manager is None:
         broadcast_manager = SocketMessageIO()
 
     user_dictionary_lock.acquire()
-    for id in user_dictionary:
-        broadcast_manager.send_message_to_socket(message, user_dictionary[id]["connection"], type=message_type)
+    for user_id in user_dictionary:
+        broadcast_manager.send_message_to_socket(message, user_dictionary[user_id]["connection"], type=message_type)
     user_dictionary_lock.release()
+
 
 def send_user_list_to_all():
     global user_dictionary_lock, user_dictionary, broadcast_manager
     if broadcast_manager is None:
         broadcast_manager = SocketMessageIO()
 
-    #develop list of online users
+    # develop list of online users
     user_dictionary_lock.acquire()
     list_info = f"{len(user_dictionary)}"
-    for id in user_dictionary:
-        print(f"{id=}\t{user_dictionary[id]}\t{user_dictionary[id]['name']=}")
-        list_info += f"\t{user_dictionary[id]['name']}"
+    for user_id in user_dictionary:
+        print(f"{user_id=}\t{user_dictionary[user_id]}\t{user_dictionary[user_id]['name']=}")
+        list_info += f"\t{user_dictionary[user_id]['name']}"
     user_dictionary_lock.release()
     # send that message to every user.
     broadcast_message_to_all(list_info, message_type=MessageType.USER_LIST)
 
-def listen_to_connection(connection:socket, id: int, address:str = None)->None:
+
+def listen_to_connection(connection_to_hear: socket, connection_id: int, connection_address: str = None) -> None:
     """
     a loop intended for a Thread to monitor the given socket and handle any messages that come from it. In this case,
     it is assumed that the first message received will be the name of the connection, in the format of a packed length
     of the name and then the name itself. All messages should be in the format of packed length + message.
-    :param connection: the socket that will be read from
-    :param address: the address of the socket (not currently used)
+    :param connection_to_hear: the socket that will be read from
+    :param connection_id: the unique id number of this user.
+    :param connection_address: the address of the socket (not currently used)
     :return: None
     """
     name = None
     manager = SocketMessageIO()
     while True:
         try:
-            type, message = manager.receive_message_from_socket(connection)
-        except (ConnectionAbortedError, ConnectionResetError) :
+            message_type, message = manager.receive_message_from_socket(connection_to_hear)
+        except (ConnectionAbortedError, ConnectionResetError):
             print(f"{name} just disconnected.")
             user_dictionary_lock.acquire()
-            del user_dictionary[id]
+            del user_dictionary[connection_id]
             user_dictionary_lock.release()
             broadcast_message_to_all(f"{'-'*6} {name} has left the conversation. {'-'*6} ")
             send_user_list_to_all()
             return
 
-        if name is None:
+        # if we got here, that means that we've received a message.
+        if name is None:  # this must be the first message, which is just the username.
             name = message
-            manager.send_message_to_socket(f"Welcome, {name}!", connection)
+            manager.send_message_to_socket(f"Welcome, {name}!", connection_to_hear)
             user_dictionary_lock.acquire()
-            user_dictionary[id]["name"] = name
+            user_dictionary[connection_id]["name"] = name
             user_dictionary_lock.release()
             broadcast_message_to_all(f"{'-'*6} {name} has joined the conversation. {'-'*6} ")
             send_user_list_to_all()
-        else:
+        else:  # it's a normal message - broadcast it to everybody.
             broadcast_message_to_all(f"{name}: {message}")
-
 
 
 if __name__ == '__main__':
@@ -69,25 +74,29 @@ if __name__ == '__main__':
     broadcast_manager = None
     latest_id = 0
 
+    # the user_dictionary is a dictionary of dictionaries of all the connected user's names & connections, keyed on
+    # unique id numbers.
+    # for example, user_dictionary might be {1: {"name":"Steve", "connection": some_socket_connection1},
+    #                                        3: {"name":"Milo", "connection": some_socket_connection2},
+    #                                        4: {"name":"Opus", "connection": some_socket_connection3}}
     user_dictionary: Dict[int, Dict] = {}
     user_dictionary_lock = threading.Lock()
 
     mySocket = socket.socket()
     port = 3000
-    mySocket.bind(('',port))
+    mySocket.bind(('', port))
     mySocket.listen(5)
-    print ("Socket is listening.")
+    print("Socket is listening.")
     while True:
-        connection, address = mySocket.accept()
+        connection, address = mySocket.accept()  # wait to receive a new socket connection.
 
-        print (f"Got connection from {address}")
+        print(f"Got connection from {address}")
 
+        # start a new thread that will continuously listen for communication from this socket connection.
         latest_id += 1
         connectionThread = threading.Thread(target=listen_to_connection, args=(connection, latest_id, address))
 
         user_dictionary_lock.acquire()
-        user_dictionary[latest_id] = {"name": "unknown", "connection":connection}
+        user_dictionary[latest_id] = {"name": "unknown", "connection": connection}
         user_dictionary_lock.release()
         connectionThread.start()
-
-
